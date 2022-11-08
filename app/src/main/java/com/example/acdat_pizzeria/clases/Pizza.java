04d72b83a0f01bfd5bdb0953cdc9556c2d671973
MJ-Curtido <manu17pc@gmail.com>
2022-11-08 13:01:24 +0100

    V1-Pizza a medias

--- app/src/main/java/com/example/acdat_pizzeria/clases/Pizza.java
+++ app/src/main/java/com/example/acdat_pizzeria/clases/Pizza.java
@@ -1,6 +1,7 @@
 package com.example.acdat_pizzeria.clases;
 
 import com.example.acdat_pizzeria.enums.TipoIngrediente;
+import com.example.acdat_pizzeria.enums.TipoNombre;
 import com.example.acdat_pizzeria.enums.TipoSalsa;
 import com.example.acdat_pizzeria.enums.TipoTamanyo;
 
@@ -14,8 +15,9 @@ public class Pizza {
     private TipoSalsa salsa;
     private ArrayList<TipoIngrediente> ingredientes;
     private Boolean favorita;
-    private String nombre;
+    private TipoNombre nombre;
     private Usuario usuario;
+    private Integer precio;
 
     public Pizza(TipoTamanyo tamanyo, TipoSalsa salsa, ArrayList<TipoIngrediente> ingredientes, Usuario usuario) {
         this.tamanyo = tamanyo;
@@ -23,13 +25,52 @@ public class Pizza {
         this.ingredientes = ingredientes;
         this.favorita = false;
         this.usuario = usuario;
-        this.nombre = "personalizada";
+        this.nombre = TipoNombre.PERSONALIZADA;
+        this.calcularPrecio();
         this.id = idTemp;
         idTemp++;
     }
 
-    public Pizza(TipoTamanyo tamanyo) {
+    public Pizza(TipoTamanyo tamanyo, TipoNombre nombre, Usuario usuario) {
+        this.tamanyo = tamanyo;
+        this.nombre = nombre;
+        this.usuario = usuario;
+        //poner para que te busque del dato la pizza predeterminada
+        this.calcularPrecio();
+    }
 
+    public Pizza(TipoSalsa salsa, ArrayList<TipoIngrediente> ingredientes) {
+        this.tamanyo = null;
+        this.salsa = salsa;
+        this.ingredientes = ingredientes;
+        this.favorita = false;
+        this.usuario = null;
+        this.nombre = TipoNombre.PERSONALIZADA;
+        this.id = idTemp;
+        idTemp++;
+    }
+
+    private void calcularPrecio() {
+        if (this.tamanyo == TipoTamanyo.PEQUEÑA) {
+            this.precio = 3;
+        }
+        else if (this.tamanyo == TipoTamanyo.MEDIANA) {
+            this.precio = 6;
+        }
+        else if (this.tamanyo == TipoTamanyo.FAMILIAR) {
+            this.precio = 12;
+        }
+        else {
+            this.precio = 17;
+        }
+
+        if (this.salsa != TipoSalsa.SIN_SALSA) {
+            this.precio += 2;
+        }
+
+        if (this.ingredientes != null) {
+            this.precio += this.ingredientes.size();
+        }
     }
 
     public void setUsuario(Usuario usuario) {
---
+++ app/src/main/java/com/example/acdat_pizzeria/servicio/Servicio.java
@@ -0,0 +1,15 @@
+package com.example.acdat_pizzeria.servicio;
+
+import com.example.acdat_pizzeria.clases.Pizza;
+import com.example.acdat_pizzeria.enums.TipoNombre;
+
+public class Servicio {
+    public Servicio() {
+    }
+
+    /*
+    public Pizza getPizzaPred(TipoNombre nombre) {
+
+    }
+    */
+}
